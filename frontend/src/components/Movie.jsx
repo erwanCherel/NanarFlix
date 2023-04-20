129ca24580a1384dfc3a122e76c786952814f614
JeanBat0202 <jeanbaptiste.blond@gmail.com>
2023-04-20 15:21:19 +0200

    finish movieList component, movies from API appear and there is button for switch page

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -1,17 +1,23 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export default function Movie({ title, image }) {
+export default function Movie({ id, title, poster_path }) {
   return (
-    <p>
-      <img className="m-auto " src={image} alt={title} />
-      <figcaption>
-        <h2 className="text-black text-center">{title}</h2>
-      </figcaption>
-    </p>
+    <Link to={`/movies/${id}`}>
+      <figure className="image-container shadow-xl">
+        <img
+          className="m-auto shadow-xl "
+          src={`https://www.themoviedb.org/t/p/w1280${poster_path}`}
+          alt={title}
+        />
+        <div className="text-overlay">{title}</div>
+      </figure>
+    </Link>
   );
 }
 
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  poster_path: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
---
+++ frontend/src/components/MovieDetails.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function MovieDetails() {
+  const [currentMovie, setCurrentMovie] = useState();
+
+  const { id } = useParams();
+
+  const getOneMovie = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${
+        import.meta.env.VITE_API_KEY
+      }&language=fr-FR`
+    )
+      .then((res) => res.json())
+      .then((data) => setCurrentMovie(data));
+  };
+
+  useEffect(() => {
+    getOneMovie();
+  }, [id]);
+
+  return (
+    <div>
+      <figure className="image-container shadow-xl">
+        <img
+          className="m-auto shadow-xl "
+          src={`https://www.themoviedb.org/t/p/w1280${currentMovie.poster_path}`}
+          alt={currentMovie.title}
+        />
+        <div className="text-overlay">{currentMovie.title}</div>
+      </figure>
+    </div>
+  );
+}
